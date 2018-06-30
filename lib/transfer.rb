0c8c31cb0f7576541185b6d2b63bfce655285dcb
Mo Al <malbazaz@mba2018.hbs.edu>
2018-06-30 20:43:43 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end 
     
   def execute_transaction
-    if self.valid?
+    if self.valid? && @sender.balance > @amount 
       @sender.balance -= @amount
       @receiver.balance += @amount 
       @status = "complete"
